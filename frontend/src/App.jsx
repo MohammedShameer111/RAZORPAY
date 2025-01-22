9439b880b347f6f0ff03d4ab8f3c4635b6868e48
MohammedShameer111 <mohamedshamir988@gmail.com>
2025-01-22 22:04:43 +0530

    update changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,27 +6,30 @@ import Product from './components/Product';
 import axios from 'axios';
 
 const App = () => {
-  const [data, setData] = useState([]);
-  const [error, setError] = useState(null);
+//   const [data, setData] = useState([]);
+//   const [error, setError] = useState(null);
 
-  // ✅ Automatically switch between local and production URLs
-  const API_BASE_URL = import.meta.env.MODE === "development"
-    ? import.meta.env.VITE_API_BASE_URL
-    : import.meta.env.VITE_API_PROD_URL;
+//   // ✅ Automatically switch between local and production URLs
+//   const API_BASE_URL = import.meta.env.MODE === "development"
+//     ? import.meta.env.VITE_API_BASE_URL
+//     : import.meta.env.VITE_API_PROD_URL;
 
-  useEffect(() => {
-    const apiUrl = `${API_BASE_URL}/api/v1/`;
+//   useEffect(() => {
+//     const apiUrl = `${API_BASE_URL}/api/v1/`;
 
-    axios.get(apiUrl)
-      .then((response) => {
-        setData(response.data);
-        console.log('Data received:', response.data);
-      })
-      .catch((error) => {
-        setError(error.message || 'Failed to fetch data');
-        console.error('Error fetching data:', error);
-      });
-  }, []);
+//     axios.get(apiUrl)
+//       .then((response) => {
+//         setData(response.data);
+//         console.log('Data received:', response.data);
+//       })
+//       .catch((error) => {
+//         setError(error.message || 'Failed to fetch data');
+//         console.error('Error fetching data:', error);
+//       });
+//   }, []);
+   
+
+
 
   return (
     <Router>
@@ -34,7 +37,7 @@ const App = () => {
         <Route path="/" element={<Product product={productsData} />} />
         <Route path="/paymentSuccess" element={<PaymentSuccess />} />
       </Routes>
-      {error && <p style={{ color: 'red', textAlign: 'center' }}>{error}</p>}
+      {/* {error && <p style={{ color: 'red', textAlign: 'center' }}>{error}</p>} */}
     </Router>
   );
 };
