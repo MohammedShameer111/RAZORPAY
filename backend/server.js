93b5bd94db46514cb1dc4ad82b1c5ee54ac9c375
MohammedShameer111 <mohamedshamir988@gmail.com>
2025-01-22 15:33:01 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import app from './app.js';
 import Razorpay from 'razorpay';
 import dotenv from 'dotenv';
 dotenv.config({path:"config/config.env"});
+const cors = require('cors');
 
 
 const port=process.env.PORT || 3000
@@ -12,7 +13,7 @@ export const instance=new Razorpay({
 })
 
 
-const cors = require('cors');
+
 app.use(cors({
   origin: 'https://razorpay-5-96rl.onrender.com',
   credentials: true,
