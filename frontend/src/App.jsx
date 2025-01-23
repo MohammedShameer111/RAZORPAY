a01fc4613cbbca70fee205a0ce4169c786379099
MohammedShameer111 <mohamedshamir988@gmail.com>
2025-01-23 11:36:15 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 
-import { HashRouter  as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter  as Router, Routes, Route } from 'react-router-dom';
 import { productsData } from './components/Data';
 import PaymentSuccess from './components/PaymentSuccess';
 import Product from './components/Product';
